bc99b465fcb2f3a5aff3b19a80f205e1e2b90a72
Mc-fly82 <flavius_marc@yahoo.fr>
2019-10-21 12:11:04 +0200

    code clean up 2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 -- décommenter pour générer des entrer au démarage de l'application
 
-insert into client values (1,'27 rue victor','82000', '123456','valentin', 'paul', 'montauban')
+-- insert into client values (1,'27 rue victor','82000', '123456','valentin', 'paul', 'montauban')
 -- insert into client values (2, '27 rue grace', '31000','123456','garcia','dylan', 'toulouse');
 -- insert into compte values (0,20000,1);
 -- insert into compte values (1,20000,1);
